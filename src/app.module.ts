78197a5059047aa46d9d7171373658d84097ac49
Leonel Fernández <leonel-leonel-1@hotmail.com>
2022-11-14 18:32:28 -0600

    Clients added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClientsModule } from './modules/clients';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    ClientsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
